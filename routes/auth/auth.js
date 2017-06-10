23b5ae2a776d714b868b741444282c405f8118a0
drgauvin <drgauvin@mgmcare.org>
2017-06-10 11:33:15 -0400

    login

--- routes/auth/auth.js
+++ routes/auth/auth.js
@@ -1,16 +1,20 @@
 const express = require('express');
-const route = express.Router()
+const route = express.Router({mergeParams: true})
 const passport = require('passport');
 const User = require('../../models/user')
 const app = express()
 
+app.use((req, res, next)=>{
+  res.locals.username = req.username
+  console.log(username);
+  next()
+})
 
 let isLoggedin = (req, res, next)=>{
   if(req.isAuthenticated()){
     return next()
   }
 }
-
 
 
 route.get('/register', (req, res)=>{
@@ -22,13 +26,15 @@ route.post('/register', (req, res)=>{
   User.register(new User(userInfo), req.body.password, (err, user)=>{
     if(err){console.log('error logging user registration');
 
-return res.render('auth')
+    return  res.render('auth', {
+        errorMessage: err.message
+      })
   }
-app.locals.username = user.username
-console.log(app.locals.username);
+
         passport.authenticate('local')(req, res, ()=>{
+            // console.log(req.user);
           res.render('voting', {
-            username: user.username
+            username: req.user.username
           })
         })
   })
@@ -42,7 +48,6 @@ route.post('/login', passport.authenticate('local', {
   successRedirect: '/voting/new',
   failureRedirect: '/login'
 }), (req, res)=>{
-  console.log(res);
 })
 
 route.get('/logout', (req, res)=>{
--- routes/removevoting.js
+++ routes/removevoting.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const route = express.Router()
+const route = express.Router({mergeParams: true})
 const votingData = require('../models/votingSchema');
 
 let isLoggedin = (req, res, next)=>{
